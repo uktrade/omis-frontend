8b8681ef15a40b449903e798bc5cb8b205fecfd7
Dom Smith <dom@teneightfive.com>
2017-09-26 12:18:33 +0100

    This adds the initial logic to accept a quote on the client app
    This adds a simple form that posts an acceptance back to the API
    and shows a confirmation page.

--- src/app/controllers/quote.js
+++ src/app/controllers/quote.js
@@ -11,12 +11,53 @@ async function renderQuote (req, res, next) {
     res.render('quote', {
       order,
       quote,
+      publicToken,
     });
   } catch (error) {
     next(error);
   }
 };
 
+async function renderAcceptedQuote (req, res, next) {
+  const publicToken = req.params.publicToken;
+  const authToken = req.session.token
+
+  try {
+    const order = await fetch(authToken, `/v3/omis/public/order/${publicToken}`);
+    const quote = await fetch(authToken, `/v3/omis/public/order/${publicToken}/quote`);
+
+    if (!quote.accepted_on) {
+      return res.redirect(`/${publicToken}`)
+    }
+
+    res.render('quote-accepted', {
+      order,
+      quote,
+      publicToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+async function acceptQuote (req, res, next) {
+  const publicToken = req.params.publicToken;
+  const authToken = req.session.token
+
+  try {
+    const accept = await fetch(authToken, {
+      method: 'post',
+      url: `/v3/omis/public/order/${publicToken}/quote/accept`,
+    });
+
+    res.redirect(`/${publicToken}/accepted`)
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   renderQuote,
+  renderAcceptedQuote,
+  acceptQuote,
 };
--- src/app/router.js
+++ src/app/router.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const indexController = require( './controllers/index' );
-const { renderQuote } = require( './controllers/quote' );
+const { renderQuote, renderAcceptedQuote, acceptQuote } = require( './controllers/quote' );
 const { setAuthToken } = require( './lib/api' );
 
 router.use(setAuthToken());
 
 router.get('/', indexController);
 router.get('/:publicToken', renderQuote);
+router.get('/:publicToken/accepted', renderAcceptedQuote);
+router.post('/:publicToken/accept', acceptQuote);
 
 module.exports = router;
