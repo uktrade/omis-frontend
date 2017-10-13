f3f5fd47aa0be1963fe5d0efe8849588968df1a2
Dom Smith <dom@teneightfive.com>
2017-10-13 15:25:39 +0100

    Add initial invoice route for customer

--- src/app/middleware/order.js
+++ src/app/middleware/order.js
@@ -1,23 +1,43 @@
+const { assign } = require('lodash')
+
 const { fetch } = require('../lib/api')
 
 async function fetchOrderDetails (req, res, next, publicToken) {
   const authToken = req.session.token
+  let order
+  let quote
+  let invoice
 
   try {
-    const order = await fetch(authToken, `/v3/omis/public/order/${publicToken}`)
-    const quote = await fetch(authToken, `/v3/omis/public/order/${publicToken}/quote`)
+    order = await fetch(authToken, `/v3/omis/public/order/${publicToken}`)
+
+    assign(order, {
+      net_cost: parseInt(order.net_cost) / 100,
+      vat_cost: parseInt(order.vat_cost) / 100,
+      total_cost: parseInt(order.total_cost) / 100,
+    })
+  } catch (error) {
+    return next(error)
+  }
+
+  try {
+    quote = await fetch(authToken, `/v3/omis/public/order/${publicToken}/quote`)
+    invoice = await fetch(authToken, `/v3/omis/public/order/${publicToken}/invoice`)
 
     quote.expired = new Date(quote.expires_on) < new Date()
-
-    res.locals = Object.assign({}, res.locals, {
-      order,
-      quote,
-      publicToken,
-    })
-    next()
   } catch (error) {
-    next(error)
+    if (error.statusCode !== 404) {
+      return next(error)
+    }
   }
+
+  res.locals = assign({}, res.locals, {
+    order,
+    quote,
+    invoice,
+    publicToken,
+  })
+  next()
 }
 
 module.exports = {
