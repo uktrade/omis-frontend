f576a79d4da614ed80996edc83225174538ee147
Dom Smith <dom@teneightfive.com>
2017-11-14 15:04:43 +0000

    Don't show invoices for legacy order

--- src/app/controllers/invoice.js
+++ src/app/controllers/invoice.js
@@ -1,4 +1,12 @@
+const { get } = require('lodash')
+
 function renderInvoice (req, res, next) {
+  const companyName = get(res.locals, 'invoice.company_name')
+
+  if (!companyName) {
+    return next()
+  }
+
   res.render('invoice')
 }
 
