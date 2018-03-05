135b3a4788d9489e35c35e1a028a3e82f753e3d5
Dom Smith <dom@teneightfive.com>
2018-03-05 16:45:44 +0000

    Only store payment gateway session ID in session
    This refactor only stores the payment gateway session ID in the
    users session rather than the whole object. It then checks the API
    again using this ID to get the latest state and payment URL of this
    payment session.
    
    This will also be used in following changes to checkt the status
    of the payment session and handle it appropriately for possible
    edge cases.

--- src/app/middleware/payment.js
+++ src/app/middleware/payment.js
@@ -27,7 +27,7 @@ function checkPaidStatus (req, res, next) {
 }
 
 async function createPaymentGatewaySession (req, res, next) {
-  if (get(req, 'paymentGatewaySession.payment_url')) {
+  if (get(req, 'paymentGatewaySession.id')) {
     return next()
   }
 
@@ -35,10 +35,30 @@ async function createPaymentGatewaySession (req, res, next) {
     const publicToken = res.locals.publicToken
     const authToken = req.session.token
 
-    req.paymentGatewaySession = await fetch(authToken, {
+    const paymentGatewaySession = await fetch(authToken, {
       method: 'post',
       url: `/v3/omis/public/order/${publicToken}/payment-gateway-session`,
     })
+
+    req.paymentGatewaySession.id = paymentGatewaySession.id
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function setPaymentGatewaySession (req, res, next) {
+  const sessionId = get(req, 'paymentGatewaySession.id')
+
+  if (!sessionId) {
+    return next()
+  }
+
+  try {
+    const publicToken = res.locals.publicToken
+    const authToken = req.session.token
+
+    res.locals.paymentGatewaySession = await fetch(authToken, `/v3/omis/public/order/${publicToken}/payment-gateway-session/${sessionId}`)
 
     next()
   } catch (error) {
@@ -46,14 +66,9 @@ async function createPaymentGatewaySession (req, res, next) {
   }
 }
 
-function setPaymentGatewayUrl (req, res, next) {
-  res.locals.paymentGatewayUrl = get(req, 'paymentGatewaySession.payment_url')
-  next()
-}
-
 module.exports = {
   checkOrderStatus,
   checkPaidStatus,
   createPaymentGatewaySession,
-  setPaymentGatewayUrl,
+  setPaymentGatewaySession,
 }
