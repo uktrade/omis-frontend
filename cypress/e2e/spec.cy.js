774afd15ab34393d20ea7a36bb16cdb2bb86ad78
Richard Pentecost <richardpentecost@DBT000444.local>
2024-03-19 12:27:36 +0000

    add e2e tests to circle config.yml

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,19 @@
 describe("template spec", () => {
-  before(() => {
-    cy.visit(
-      "http://localhost:4000/7AGZC7uaAIV-5L34J5lnZXHvFG9J1xnbBQnieAUCn-LRJpr-QA"
-    );
-  });
+  // before(() => {
+  //   cy.visit(
+  //     "http://localhost:4000/7AGZC7uaAIV-5L34J5lnZXHvFG9J1xnbBQnieAUCn-LRJpr-QA"
+  //   );
+  // });
 
-  it("passes", () => {
-    cy.get("h1").should("exist").and("have.text", "Your order");
-    cy.get('[class="button"]')
-      .should("exist")
-      .and("contain", "Review your quote");
+  // it("passes", () => {
+  //   cy.get("h1").should("exist").and("have.text", "Your order");
+  //   cy.get('[class="button"]')
+  //     .should("exist")
+  //     .and("contain", "Review your quote");
+  // });
+
+  it("visits OMIS root page", () => {
+    cy.visit("http://localhost:4000");
+    cy.get("h1").should("exist").and("have.text", "About this service");
   });
 });
