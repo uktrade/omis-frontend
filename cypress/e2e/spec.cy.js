9f66c52b08b34f5ad93814e6ce94720a0ddaa663
Richard Pentecost <richardpentecost@DBT000444.local>
2024-03-19 14:40:00 +0000

    change workflows in config.yml

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,16 @@
 describe("template spec", () => {
-  // before(() => {
-  //   cy.visit(
-  //     "http://localhost:4000/7AGZC7uaAIV-5L34J5lnZXHvFG9J1xnbBQnieAUCn-LRJpr-QA"
-  //   );
-  // });
-
-  // it("passes", () => {
-  //   cy.get("h1").should("exist").and("have.text", "Your order");
-  //   cy.get('[class="button"]')
-  //     .should("exist")
-  //     .and("contain", "Review your quote");
-  // });
-
   it("visits OMIS root page", () => {
     cy.visit("http://localhost:4000");
     cy.get("h1").should("exist").and("have.text", "About this service");
+  });
+
+  it("visits OMIS order page", () => {
+    cy.visit(
+      "http://localhost:4000/7AGZC7uaAIV-5L34J5lnZXHvFG9J1xnbBQnieAUCn-LRJpr-QA"
+    );
+    cy.get("h1").should("exist").and("have.text", "Your order");
+    cy.get('[class="button"]')
+      .should("exist")
+      .and("contain", "Review your quote");
   });
 });
