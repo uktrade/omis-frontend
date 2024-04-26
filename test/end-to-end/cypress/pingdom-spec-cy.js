6bea2b9552e48863a09287a866b1a954fd3d4d23
Pippo Raimondi <piporamaa@gmail.com>
2024-04-26 09:55:46 +0100

    Negative scenario for pingdom endpoint

--- test/end-to-end/cypress/pingdom-spec-cy.js
+++ test/end-to-end/cypress/pingdom-spec-cy.js
@@ -5,4 +5,19 @@ describe("Pingdom", () => {
       expect(response.body).to.contain("OK");
     });
   });
+
+  it("Health check returns 503 when a dependency fails", () => {
+    cy.intercept("GET", "/ping.xml", {
+      statusCode: 500,
+      body: "Internal Server Error"
+    }).as("dependencyFail");
+
+    cy.request({
+      url: "http://localhost:4000/ping.xml",
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(503);
+      expect(response.body).to.contain("Service Unavailable");
+    });
+  });
 });
