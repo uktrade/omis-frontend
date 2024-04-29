a7f74cc5a580e961fb768b017b6802aedaa17807
Pippo Raimondi <piporamaa@gmail.com>
2024-04-29 07:53:04 +0100

    Add unit test for pingdom

--- test/end-to-end/cypress/pingdom-spec-cy.js
+++ test/end-to-end/cypress/pingdom-spec-cy.js
@@ -1,23 +1,8 @@
-describe("Pingdom", () => {
+describe("Pingdom - Success", () => {
   it("Health check returns Status - OK", () => {
     cy.request("http://localhost:4000/ping.xml").then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.contain("OK");
-    });
-  });
-
-  it("Health check returns 503 when a dependency fails", () => {
-    cy.intercept("GET", "/ping.xml", {
-      statusCode: 500,
-      body: "Internal Server Error"
-    }).as("dependencyFail");
-
-    cy.request({
-      url: "http://localhost:4000/ping.xml",
-      failOnStatusCode: false
-    }).then((response) => {
-      expect(response.status).to.eq(503);
-      expect(response.body).to.contain("Service Unavailable");
     });
   });
 });
