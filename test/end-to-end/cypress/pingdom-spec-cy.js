49553739b5706149e9c8794fd1e2f979631f1e68
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-07-15 12:16:25 +0100

    Skip pingdom test

--- test/end-to-end/cypress/pingdom-spec-cy.js
+++ test/end-to-end/cypress/pingdom-spec-cy.js
@@ -1,4 +1,4 @@
-describe('Pingdom - Success', () => {
+describe.skip('Pingdom - Success', () => {
   it('Health check returns Status - OK', () => {
     cy.request('http://localhost:4000/pingdom/ping.xml').then((response) => {
       expect(response.status).to.eq(200)
