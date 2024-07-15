e655f9a7ad7d09b23570d109ca6499c8fbe25b21
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-07-15 12:04:27 +0100

    Update healthcheck in test

--- test/end-to-end/cypress/pingdom-spec-cy.js
+++ test/end-to-end/cypress/pingdom-spec-cy.js
@@ -1,6 +1,6 @@
 describe('Pingdom - Success', () => {
   it('Health check returns Status - OK', () => {
-    cy.request('http://localhost:4000/ping.xml').then((response) => {
+    cy.request('http://localhost:4000/pingdom/ping.xml').then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).to.contain('OK')
     })
